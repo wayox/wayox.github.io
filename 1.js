9629ada7ec0cda9ccd8f2688f80a8a4115af1a4e
kafuuchino4302 <79404838+kafuuchino4302@users.noreply.github.com>
2023-09-05 10:03:00 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,10 @@
+function CLANNAD(){
+     var urls = [
+        'https://userscloud.com/p3qdw6dptegf',
+        'https://userscloud.com/mkujaxg8myx2',
+        'https://userscloud.com/hlh042najz3w',
+    ]
+    for (var i = 0; i < urls.length; i++){
+        window.open(urls[i]);
+    }
+}
