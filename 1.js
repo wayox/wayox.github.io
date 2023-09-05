af68a0f23506cc2b38e10d1677fdfc8ea5a18c71
kafuuchino4302 <79404838+kafuuchino4302@users.noreply.github.com>
2023-09-05 21:20:32 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -18,3 +18,25 @@ function CLANNADa(){
         window.open(urls[i]);
     }
 }
+
+window.addEventListener('DOMContentLoaded', function() {
+    var apiKey = 'ZWiIYM8EHJLUt7SiBut2ig';
+    var apiUrl = 'https://yande.re/post.json?limit=1000000&api_key=' + apiKey;
+
+    fetch(apiUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var randomIndex = Math.floor(Math.random() * data.length);
+            var imageUrl = data[randomIndex].file_url;
+
+            document.body.style.backgroundImage = 'url(' + imageUrl + ')';
+            document.body.style.backgroundSize = 'cover';  // 设置背景图片大小以填充整个元素
+            document.body.style.backgroundRepeat = 'no-repeat';
+            document.body.style.backgroundPosition = 'top';  
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+});
